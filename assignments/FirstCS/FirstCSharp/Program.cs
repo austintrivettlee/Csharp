1714d5b7f28130e2ccbd9cb8aa28c664b1164590
Austin Trivett Lee <138937655+austintrivettlee@users.noreply.github.com>
2023-09-25 11:56:41 -0500

    fundamentals

---
+++ assignments/FirstCS/FirstCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 1; i < 256; i++)
+{
+    Console.WriteLine(i);
+}
+
+Random rand = new Random();
+int total = 0;
+for (int i = 0; i <= 6; i++)
+{
+    total = total + (rand.Next(10, 21));
+}
+
+Console.WriteLine(total);
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    if (i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+        
+    }
+}
