5c6d9ce49d68544b3d31becb8e5bf49e176704dc
Austin Trivett Lee <138937655+austintrivettlee@users.noreply.github.com>
2023-09-26 13:47:41 -0500

    fund2

---
+++ assignments/Fundamentals-II/Program.cs
@@ -0,0 +1,57 @@
+﻿int[] numArray = new int[5];
+int[] firstArray = {1,2,3,4,5};
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+Console.WriteLine(firstArray[i]);
+}
+
+Console.WriteLine("----------------------");
+
+string[] names = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+
+for (int i = 0; i < names.Length; i++)
+{
+Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("----------------------");
+bool[] boolArray = new bool[10];
+
+for (int i = 0; i < boolArray.Length; i++)
+{
+    boolArray[i] = i % 2 == 0;
+}
+for (int i = 0; i < boolArray.Length; i++)
+{
+    Console.WriteLine(boolArray[i]);
+}
+Console.WriteLine("----------------------");
+    
+List<string> iceCreamFlav = new List<string> {"Mint", "Vanilla", "Chocolate", "Strawberry", "Mango"};
+
+Console.WriteLine(iceCreamFlav.Count);
+Console.WriteLine(iceCreamFlav[2]);
+
+iceCreamFlav.RemoveAt(0);
+Console.WriteLine("----------------------");
+
+for (int i = 0; i < iceCreamFlav.Count; i++)
+{
+    Console.WriteLine(iceCreamFlav[i]);
+}
+Console.WriteLine("----------------------");
+Console.WriteLine(iceCreamFlav.Count);
+Console.WriteLine("----------------------");
+
+Dictionary<string,string> favIceCream = new Dictionary<string, string>();
+Random rand = new Random();
+for (int i = 0; i < names.Length; i++)
+{
+    favIceCream.Add(names[i], iceCreamFlav[rand.Next(0,4)]);
+}
+
+foreach(KeyValuePair<string,string> entry in favIceCream)
+{
+    Console.WriteLine($"{entry.Key} - {entry.Value}");
+}
