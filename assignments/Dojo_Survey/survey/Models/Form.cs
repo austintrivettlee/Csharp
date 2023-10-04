52f62b335830ae24d5ac763335e73afdb87b3dcf
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-10-04 08:44:57 -0500

    valid

--- assignments/Dojo_Survey/survey/Models/Form.cs
+++ assignments/Dojo_Survey/survey/Models/Form.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 namespace SurveyApp.Models
 {
     public class FormModel
     {
-        public string Name { get; set; }
-        public string DojoLocation { get; set; }
-        public string FavoriteLanguage { get; set; }
-        public string Comment { get; set; }
+        [Required(ErrorMessage="Name is required!")]
+        [MinLength(3, ErrorMessage="Name must be at least 3 characters in length.")]
+        public string? Name { get; set; }
+        [Required]
+        public string? DojoLocation { get; set; }
+        [Required]
+        public string? FavoriteLanguage { get; set; }
+        [MinLength(20, ErrorMessage="Comment must be at least 20 characters in length.")]
+        public string? Comment { get; set; }
     }
 }
